efde71572ae45f8150f7c8804abdfdcb6593d4d7
PadhleYrr <moliage490@gmail.com>
2026-09-09 22:12:48 +0530

    unzipgit

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        maven("https://jitpack.io") // Add this
+
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // Include Shizuku's Maven repository
+    }
+}
+
+rootProject.name = "My Application"
+include(":app")
